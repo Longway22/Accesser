973a91785aba7f1b0339aadfe79f8ed1533a8676
Yogas Kung <yogaskung@protonmail.ch>
2021-02-07 10:51:26 +0000

    update rules for githubusercontent.com

--- accesser.py
+++ accesser.py
@@ -57,9 +57,13 @@ class ProxyHandler(StreamRequestHandler):
         if "." in res.subdomain:
             server_name = res.subdomain.split('.', 1)[-1] + '.' + res.fld
         else:
-            server_name = res.fld
-            if "githubusercontent.com" in server_name:
-                server_name = "githubusercontent.com"
+               server_name = res.fld
+               if "actions.githubusercontent.com" in server_name:
+                   server_name = "actions.githubusercontent.com"
+               else:
+                       server_name = res.fld
+                       if "githubusercontent.com" in server_name:
+                           server_name = "githubusercontent.com"
         with cert_lock:
             if not server_name in cert_store:
                 cm.create_certificate(server_name)
